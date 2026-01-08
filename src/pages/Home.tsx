f5e352910502293236a783f7cd4dee6890fba85d
Aldo Herrera <aldonvacriates@gmail.com>
2026-01-08 13:55:42 -0700

    api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
-import type { Product } from "../types/types";
+import type { Category, Product } from "../types/types";
 import ProductCard from "../commponents/ProductCard";
 import { useProductContext } from "../context/ProductContext";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { fetchProducts } from "../api/api";
+import { fetchProducts, fetchCategories } from "../api/api";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
-  const { products, dispatch } = useProductContext();
+  const { products, selectedCategory, dispatch } = useProductContext();
 
   const {
     data: productsData,
@@ -24,21 +24,41 @@ const Home: React.FC = () => {
       dispatch({ type: "SET_PRODUCTS", payload: productsData.data });
   }, [dispatch, productsData]);
 
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     const response = await fetch("https://fakestoreapi.com/products");
-  //     const data: Product[] = await response.json();
-  //     dispatch({ type: "SET_PRODUCTS", payload: data });
-  //   };
-  //   fetchProducts();
-  // }, [dispatch]);
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+
+  const getFilteredProducts = () => {
+    if (selectedCategory) {
+      return products.filter(
+        (product: Product) => product.category === selectedCategory
+      );
+    }
+    return products;
+  };
+
+  const filteredProducts = getFilteredProducts();
 
   return (
     <div>
-      {isLoading && <h1>Loading products...</h1>}
+      <select
+        className="m-3 p-2 border rounded"
+        onChange={(e) =>
+          dispatch({ type: "SET_SELECTED_CATEGORY", payload: e.target.value })
+        }
+      >
+        <option value="">All Categories</option>
+        {categories?.data.map((category: Category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
       <button onClick={() => navigate("/profile")}>Go to Profile Page</button>
+      {isLoading && <h1>Loading products...</h1>}
       <div className="d-flex flex-wrap gap-4 justify-content-center">
-        {products.map((product: Product) => (
+        {filteredProducts.map((product: Product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
